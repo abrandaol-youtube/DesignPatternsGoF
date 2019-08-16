f2af0c08744e3b23c6b0a872fb9eb83bd469f3b4
Alexandre <alexandre.brandao.lustosa@gmail.com>
2019-08-16 20:05:55 -0300

    Adicionado prototype

---
+++ PrototypeDesignPattern/Carro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeDesignPattern
+{
+    public sealed class Carro
+    {
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public string Ano { get; set; }
+
+        public Carro ShallowCopy()
+        {
+            return (Carro) this.MemberwiseClone();
+        }
+
+        public Carro DeepCopy()
+        {
+            return new Carro
+            {
+                Ano = string.Copy(this.Ano),
+                Marca = string.Copy(this.Marca),
+                Modelo = string.Copy(this.Modelo)
+            };
+        }
+    }
+}
---
+++ PrototypeDesignPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PrototypeDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carro1 = new Carro {Ano = "2001", Marca = "Fiat", Modelo = "Uno"};
+
+            var carro2 = carro1;
+            var carro3 = carro1.ShallowCopy();
+            var carro4 = carro1.DeepCopy();
+
+            Console.WriteLine($"{carro1.Modelo} | {carro1.Marca} | {carro1.Ano}");
+
+            carro2.Ano = "2015";
+            carro3.Modelo = "GOL";
+
+            Console.WriteLine($"{carro1.Modelo} | {carro1.Marca} | {carro1.Ano}");
+
+            Console.WriteLine("------");
+        }
+
+
+    }
+}
