e6cdbf552258ec949efdf2e1e011914566c3b62f
Alexandre <alexandre.brandao.lustosa@gmail.com>
2019-09-15 20:58:08 -0300

    Adicionado projeto Builder

---
+++ BuilderDesignPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BuilderDesignPattern
+{
+    /*
+        Intenção
+
+            - "Separar a construção de um obejto complexo de sua representação de modo que 
+                o processo de construção possa criar diferentes representações".
+     
+        Vantagens do padrão Builder
+        
+        - O padrão builder permite ao desenvolvedor esconder detalhes de como os objetos são criados
+        - O padrão Builder permite ao desenvolvedor um grande variedade de repsoentações internas do objeto a ser construído
+        - Cada Builder é independente de outros e do restante da aplicação
+            - provê modularidade
+            - simplifica a adição no novos Builders
+        - Provê grande controle sobre a criação de objetos complexos     
+     */
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello nerds!");
+        }
+    }
+}
